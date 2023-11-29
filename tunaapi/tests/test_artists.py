cfeff49dce6b52016cc4b8c73384db3938a05475
Trinity Christiana <trinitycterry@gmail.com>
2023-11-29 02:24:48 +0000

    added teset files

---
+++ tunaapi/tests/test_artists.py
@@ -0,0 +1,12 @@
+import unittest
+from rest_framework.test import APIRequestFactory
+
+
+class TestArtists(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = APIRequestFactory()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/customer/details/")
