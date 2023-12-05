2bd60a424ca454f35e96996aa1177ea383a7f5af
Trinity Christiana <trinitycterry@gmail.com>
2023-12-05 00:52:15 +0000

    Updated branch with stuff

--- tunaapi/tests/test_genres.py
+++ tunaapi/tests/test_genres.py
@@ -1,12 +1,24 @@
 import unittest
+import pytest
+
 from rest_framework.test import APIRequestFactory
 
-
+@pytest.mark.skip()
 class TestGenres(unittest.TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = APIRequestFactory()
 
+    def test_create(self):
+        response = self.client.get("")
+    def test_delete(self):
+        response = self.client.get("")
+    def test_update(self):
+        response = self.client.get("")
+    def test_list(self):
+        response = self.client.get("")
     def test_details(self):
-        # Issue a GET request.
-        response = self.client.get("/customer/details/")
+        response = self.client.get("")
+
+    # def test_stretch_popular_genres(self):
+    #     response = self.client.get("")
